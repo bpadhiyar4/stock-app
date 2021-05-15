7d4a2361c0b64811434aa6843e062836fa834c29
Bharat Padhiyar <bharatkumar.padhiyar@thomsonreuters.com>
2021-05-15 19:39:27 +0530

    Methods added in the interface

--- src/main/java/com/learningrest/stockapp/service/IStockMarketSerivice.java
+++ src/main/java/com/learningrest/stockapp/service/IStockMarketSerivice.java
@@ -4,6 +4,32 @@ import com.learningrest.stockapp.model.Intraday;
 
 public interface IStockMarketSerivice {
 
+    /**
+     * method to retrieves the intraday trading information.
+     * @param stock for which we need an intraday stock info.
+     * @return All intraday trading object.
+     */
     Intraday[] getIntradayTrades(String stock);
-    
+
+    /**
+     * Call the api implemented in StockMarketRepository and return the total sum of
+     * numberofTrades.
+     */
+    int getTotalNumberOfTrades(String stock);
+
+    /**
+     * Get the lowest trade price from the Intraday trading.
+     * 
+     * @param stock for which we need to find the lowest price.
+     * @return lowest price.
+     */
+    double getLowestTradePrice(String stock);
+
+    /**
+     * Get the highest trade price from the Intraday trading.
+     * 
+     * @param atock for which we need to find the lowest price.
+     * @return highest price.
+     */
+    double getHighestTradePrice(String atock);
 }
--- src/main/java/com/learningrest/stockapp/service/StockMarketService.java
+++ src/main/java/com/learningrest/stockapp/service/StockMarketService.java
@@ -3,23 +3,60 @@ package com.learningrest.stockapp.service;
 import com.learningrest.stockapp.model.Intraday;
 import com.learningrest.stockapp.repository.IStockMarketRepository;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StockMarketService implements IStockMarketSerivice{
-
+public class StockMarketService implements IStockMarketSerivice {
 
     private IStockMarketRepository stockMarketRepository;
 
     /**
-     * 
+     * Constructor
      */
     public StockMarketService(IStockMarketRepository stockMarketRepository) {
         this.stockMarketRepository = stockMarketRepository;
     }
 
+    /**
+     * Call the api implemented in StockMarketRepository and return the total sum of
+     * numberofTrades.
+     */
     @Override
     public Intraday[] getIntradayTrades(String stock) {
         return this.stockMarketRepository.getIntradayTrades(stock);
+    }
+
+    /**
+     * Call the api implemented in StockMarketRepository and return the total sum of
+     * numberofTrades.
+     * @param stock name for which we need total number of trades.
+     * @return Total NUmber of trades of Intraday trading.
+     */
+    @Override
+    public int getTotalNumberOfTrades(String stock) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Get the lowest trade price from the Intraday trading.
+     * 
+     * @param stock for which we need to find the lowest price.
+     * @return lowest price.
+     */
+    @Override
+    public double getLowestTradePrice(String stock) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Get the highest trade price from the Intraday trading.
+     * 
+     * @param atock for which we need to find the lowest price.
+     * @return highest price.
+     */
+    @Override
+    public double getHighestTradePrice(String atock) {
+        throw new NotImplementedException();
     }
 }
